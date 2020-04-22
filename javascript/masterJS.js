a6d87a5108afee3515ca9186d6f2c8cd70d5347a
Jake Green <jgreen47@byui.edu>
2020-04-22 14:04:23 -0500

    example for week 1

--- javascript/masterJS.js
+++ javascript/masterJS.js
@@ -1,12 +1,14 @@
 function saveStory() {
   var storyName = document.getElementById("story_name").value;
   var storyHTML = document.getElementById('story_editor').value;
+  console.log(storyHTML);
   localStorage.setItem(storyName, storyHTML);
 }
 
 function loadStory() {
   var storyName = document.getElementById('story_name');
   var storyHTML = localStorage.getItem(storyName);
+  console.log(storyHTML);
   document.getElementById('story_editor').value = storyHTML;
 }
 
