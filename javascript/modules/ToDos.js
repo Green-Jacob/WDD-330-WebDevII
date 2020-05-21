05d00b019b9ebdd5ec869843bffbbe2be54fa80b
Jake Green <jgreen47@byui.edu>
2020-05-21 13:03:32 -0500

    Update ToDos.js

--- javascript/modules/ToDos.js
+++ javascript/modules/ToDos.js
@@ -1,8 +1,7 @@
-class listItem {
+export class listItem {
   constructor(i, c, complete) {
     this.idNum = i;
     this.content = c;
     this.completed = complete;
   }
 }
-export default listItem;
