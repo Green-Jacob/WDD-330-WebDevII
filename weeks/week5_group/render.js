2e09dcd8b4cea3dbd38da3cd6618f0da8a4012b8
Jake Green <jgreen47@byui.edu>
2020-05-20 20:10:41 -0500

    Update render.js

--- weeks/week5_group/render.js
+++ weeks/week5_group/render.js
@@ -1,8 +1,8 @@
+const imgBasePath = "//byui-cit.github.io/cit261/examples/";
 export default class Render {
   constructor(){
 
   }
-  const imgBasePath = "//byui-cit.github.io/cit261/examples/";
   renderOneHike(hike) {
     const item = document.createElement("li");
 
