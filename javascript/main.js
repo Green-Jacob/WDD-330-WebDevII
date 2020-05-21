a36e6764fb35f3c0945c0e995dcf784e5b0a67ec
Jake Green <jgreen47@byui.edu>
2020-05-21 13:15:05 -0500

    Update main.js

--- javascript/main.js
+++ javascript/main.js
@@ -1,23 +1,23 @@
-import listItem from "./modules/ToDos.js";
-import storageManager from "./modules/ls.js";
-import util from "./modules/utilities.js";
+import listItem from "./modules/ToDos.js"
+import storageManager from "./modules/ls.js"
+import util from "./modules/utilities.js"
 
-var utility = new util;
+var utility = new util
 
 function tryClass() {
-  var lI = new listItem(12, "Hello World!", true);
-  console.log(lI);
+  var lI = new listItem(12, "Hello World!", true)
+  console.log(lI)
 }
 
 function showList() {
-  var listArea = document.getElementById('listArea');
-  var list = "";
+  var listArea = document.getElementById('listArea')
+  var list = ""
   let listItem = '<section class="todo-item"><input type="checkbox" name="done" value="">Task Name here<button type="button" name="delete">X</button><hr><br></section>'
   for (var i = 0; i < 5; i++) {
-    list += listItem;
+    list += listItem
   }
   listArea.innerHTML = list;
-  var buttons = document.getElementsByName('delete');
+  var buttons = document.getElementsByName('delete')
   for (var i = 0; i < buttons.length; i++) {
     buttons[i].addEventListener('click', utility.deleteItem)
   }
