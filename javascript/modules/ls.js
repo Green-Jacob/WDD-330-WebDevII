295c7ac2fb872cf8ff78c9bdeb81fd8f33af1ba9
Jake Green <jgreen47@byui.edu>
2020-05-26 16:08:18 -0500

    Update ls.js

--- javascript/modules/ls.js
+++ javascript/modules/ls.js
@@ -13,7 +13,19 @@ export default class storageManager {
     var list = [];
     var string = window.localStorage.getItem(key)
     console.log(string);
-    
+    JSON.parse(string, function(key, value){
+      var li = new listItem;
+      if (key == "idNum") {
+        li.idNum = value;
+      }
+      if (key == "content") {
+        li.content = value;
+      }
+      if (key == "completed") {
+        li.completed = value;
+      }
+      list.push(li)
+    });
     return (list);
   }
 }
