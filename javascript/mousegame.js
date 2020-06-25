9f91fd2627acd1dff0a1da20d4ca56f3baf3b105
Jake Green <jgreen47@byui.edu>
2020-06-25 14:08:03 -0500

    Update mousegame.js

--- javascript/mousegame.js
+++ javascript/mousegame.js
@@ -2,6 +2,6 @@ import imageFetcher from "./modules/imageFetcher.js";
 const fetcher = new imageFetcher;
 document.getElementById('button').addEventListener('click', function(){
   var obj = []
-  fetcher.requestImage("yellow+flower", obj)
+  obj = fetcher.requestImage("yellow+flower", obj)
   console.log(obj)
 });
