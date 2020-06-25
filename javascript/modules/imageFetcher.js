3a4d305c3e3c91bf1f0a3f2040037797cea80731
Jake Green <jgreen47@byui.edu>
2020-06-25 14:18:48 -0500

    Update imageFetcher.js

--- javascript/modules/imageFetcher.js
+++ javascript/modules/imageFetcher.js
@@ -3,16 +3,17 @@ export default class imageFetcher{
   {
     debugger
     console.log(data);
+    return data.hits;
   }
   //free API key, secure to put into client side JS
   requestImage(string)
   {
     var url = "https://pixabay.com/api/?key=17090702-3e1393b7e7a169d0b54bf7628&safesearch=true";
     var search = url + "&q=" + string;
-    var a
+    var a []
     fetch(search)
       .then(response => response.json())
-      .then(data => this.getData(data))
+      .then(data => a = this.getData(data))
       .catch((error) => {
         console.log('Error:', error);
       })
