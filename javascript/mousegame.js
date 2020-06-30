d38d7ef98a816b6e6c2ea2731987445e074ee528
Jake Green <jgreen47@byui.edu>
2020-06-30 13:49:32 -0500

    Update mousegame.js

--- javascript/mousegame.js
+++ javascript/mousegame.js
@@ -1,5 +1,19 @@
 import imageFetcher from "./modules/imageFetcher.js";
+import storageManager from "./modules/ls.js"
+
 const fetcher = new imageFetcher;
+const storage = new storageManager;
+
+function showPictures() {
+  var matcher = storage.getImage("match");
+  var photos = storage.getImage("option1");
+  photos += storage.getImage("option2");
+  photos += storage.getImage("option3");
+  document.getElementById('matcher').innerHTML = matcher;
+  document.getElementById('photoArea').innerHTML = photos;
+}
+
 document.getElementById('button').addEventListener('click', function(){
   fetcher.requestImage();
+  showPictures();
 });
