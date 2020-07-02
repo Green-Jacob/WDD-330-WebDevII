3537f1f24842646539e5715710efbebf2f3f10f7
Jake Green <jgreen47@byui.edu>
2020-07-02 13:26:21 -0500

    Update mousegame.js

--- javascript/mousegame.js
+++ javascript/mousegame.js
@@ -4,6 +4,8 @@ import Game from "./modules/game.js"
 const fetcher = new imageFetcher;
 const storage = new storageManager;
 
+let retrieve = async function() { fetcher.requestImage(); }
+
 function showPictures() {
   var matcher = storage.getImage("match");
   var photos = storage.getImage("option1");
@@ -20,13 +22,39 @@ function beginGame()
   var o3 = document.getElementById('option3');
   var m = document.getElementsByClassName('matcherPicture')[0];
   var game = new Game(m, o1, o2, o3);
-  o1.addEventListener('click', function(){game.click(o1)});
-  o2.addEventListener('click', function(){game.click(o2)});
-  o3.addEventListener('click', function(){game.click(o3)});
+  o1.addEventListener('click', function(){
+    let result = game.click(o1);
+    if (result == true) {
+      retrieve().then((value) =>
+      {
+        showPictures();
+        beginGame();
+      });
+    }
+  });
+  o2.addEventListener('click', function(){
+    let result = game.click(o2);
+    if (result == true) {
+      retrieve().then((value) =>
+      {
+        showPictures();
+        beginGame();
+      });
+    }
+  });
+  o3.addEventListener('click', function(){
+    let result = game.click(o3);
+    if (result == true) {
+      retrieve().then((value) =>
+      {
+        showPictures();
+        beginGame();
+      });
+    }
+  });
 }
 
 document.getElementById('button').addEventListener('click', function(){
-  let retrieve = async function() { fetcher.requestImage(); }
   retrieve().then((value) =>
   {
     showPictures();
