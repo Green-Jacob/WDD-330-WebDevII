5e981f49fa7430886e25ce3f3a4c942bc307cdea
Jake Green <jgreen47@byui.edu>
2020-07-02 13:02:25 -0500

    Update game.js

--- javascript/modules/game.js
+++ javascript/modules/game.js
@@ -1,17 +1,17 @@
 export default class Game{
   constructor(match, o1, o2, o3)
   {
-    this.match = match.value;
-    this.opt1 = o1.value;
-    this.opt2 = o2.value;
-    this.opt3 = o3.value;
+    this.match = match;
+    this.opt1 = o1;
+    this.opt2 = o2;
+    this.opt3 = o3;
   }
 
   click(option)
   {
     console.log(option.value);
-    console.log(this.match);
-    if (option.value == this.match)
+    console.log(this.match.value);
+    if (option.value == this.match.value)
     {
       console.log("match!!");
     }
